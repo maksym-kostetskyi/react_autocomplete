e53397760f28dbb48232947806d029b5db490649
Maksym Kostetskyi <mkost@gmail.com>
2025-02-27 16:06:08 +0200

    solution

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,131 @@
-import React from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import './App.scss';
 import { peopleFromServer } from './data/people';
+import debounce from 'lodash.debounce';
+import { Person } from './types/Person';
 
 export const App: React.FC = () => {
-  const { name, born, died } = peopleFromServer[0];
+  const peopleCopy = [...peopleFromServer];
+  const [selectedHuman, setSelectedHuman] = useState<Person | null>(null);
+  const [query, setQuery] = useState('');
+  const [inputIsActive, setInputIsActive] = useState(false);
+  const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
+  const inputField = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    const handleFocus = () => setInputIsActive(true);
+    const handleBlur = () => setInputIsActive(false);
+    const inputFieldCurrent = inputField.current;
+
+    if (inputFieldCurrent) {
+      inputFieldCurrent.addEventListener('focus', handleFocus);
+      inputFieldCurrent.addEventListener('blur', handleBlur);
+    }
+
+    return () => {
+      if (inputFieldCurrent) {
+        inputFieldCurrent.removeEventListener('focus', handleFocus);
+        inputFieldCurrent.removeEventListener('blur', handleBlur);
+      }
+    };
+  }, []);
+
+  const applyQuery = useCallback(debounce(setQuery, 2000), []);
+
+  const handleQueryChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (!peopleCopy.some(human => human.name === e.target.value)) {
+      setSelectedHuman(null);
+    } else {
+      setSelectedHuman(
+        peopleCopy.find(human => human.name === e.target.value) || null,
+      );
+    }
+
+    setQuery(e.target.value);
+    applyQuery(e.target.value);
+  };
+
+  const filteredPeople = useMemo(() => {
+    return peopleCopy.filter(human =>
+      human.name.toLowerCase().includes(query.toLowerCase()),
+    );
+  }, [query, peopleCopy]);
+
+  const handleSelect = (nameProp: string) => {
+    setQuery(nameProp);
+    setInputIsActive(false);
+    setSelectedHuman(peopleCopy.find(human => human.name === nameProp) || null);
+  };
 
   return (
     <div className="container">
       <main className="section is-flex is-flex-direction-column">
         <h1 className="title" data-cy="title">
-          {`${name} (${born} - ${died})`}
+          {selectedHuman !== null
+            ? `${selectedHuman?.name} (${selectedHuman?.born} - ${selectedHuman?.died})`
+            : `No selected person`}
         </h1>
 
         <div className="dropdown is-active">
           <div className="dropdown-trigger">
             <input
               type="text"
+              value={query}
+              onChange={e => handleQueryChange(e)}
               placeholder="Enter a part of the name"
               className="input"
               data-cy="search-input"
+              ref={inputField}
             />
           </div>
 
           <div className="dropdown-menu" role="menu" data-cy="suggestions-list">
-            <div className="dropdown-content">
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Haverbeke</p>
+            {filteredPeople.length !== 0 && inputIsActive && (
+              <div className="dropdown-content">
+                {filteredPeople.map((human, index) => (
+                  <div
+                    className={'dropdown-item'}
+                    data-cy="suggestion-item"
+                    key={human.name}
+                    onMouseDown={() => handleSelect(human.name)}
+                    onMouseEnter={() => setHoveredIndex(index)}
+                    onMouseLeave={() => setHoveredIndex(null)}
+                  >
+                    <p
+                      className={`has-text-link' ${
+                        hoveredIndex === index ? 'has-text-danger' : ''
+                      }`}
+                    >
+                      {human.name}
+                    </p>
+                  </div>
+                ))}
               </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Bernard Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter Antone Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Haverbeke</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-link">Pieter de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Petronella de Decker</p>
-              </div>
-
-              <div className="dropdown-item" data-cy="suggestion-item">
-                <p className="has-text-danger">Elisabeth Hercke</p>
-              </div>
-            </div>
+            )}
           </div>
         </div>
 
-        <div
-          className="
+        {filteredPeople.length === 0 && (
+          <div
+            className="
             notification
             is-danger
             is-light
             mt-3
             is-align-self-flex-start
           "
-          role="alert"
-          data-cy="no-suggestions-message"
-        >
-          <p className="has-text-danger">No matching suggestions</p>
-        </div>
+            role="alert"
+            data-cy="no-suggestions-message"
+          >
+            <p className="has-text-danger">No matching suggestions</p>
+          </div>
+        )}
       </main>
     </div>
   );
